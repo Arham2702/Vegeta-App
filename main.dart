e8e0774fd2007a18fb17fa944d84549cc4c2f763
Arham Asif Syed <86039870+Arham2702@users.noreply.github.com>
2023-05-11 21:45:53 +0530

    updated code

--- main.dart
+++ main.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black38,
-        appBar: AppBar(
-          title: Text('Vegeta'),
-          backgroundColor: Colors.lightBlueAccent,
-        ),
-        body: Center(
-          child: Image(
-            image: AssetImage('images/vegeta.jpg'),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/vegeta.jpg'),
+                radius: 65,
+              ),
+              Text(
+                'Vegeta',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Prompt'),
+              ),
+              Text(
+                'G.O.A.T',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                  fontFamily:'CastoroTitling',
+                  letterSpacing: 4
+                ),
+              )
+            ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
